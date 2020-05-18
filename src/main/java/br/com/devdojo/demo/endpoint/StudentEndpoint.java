eec5f7ba3f105b35620eb71fe328a2a4f7d2fb30
Jose Augusto de Lima Pereira <josealp@disot.com.br>
2020-05-18 19:17:57 -0300

    DDSB-06 REST e POST aula 2

--- src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
@@ -6,14 +6,10 @@ import br.com.devdojo.demo.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 /**
- * @author: Jose Augusto for DevDojo SpringBoot Course on 5/18/20
+ * author: Jose Augusto for DevDojo SpringBoot Course on 5/18/20
  */
 
 @RestController
@@ -37,4 +33,9 @@ public class StudentEndpoint {
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
 }
